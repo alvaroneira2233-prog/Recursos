96fa34847d81a1adf88b3621578ac86f0ecdd0d3
alvaro <alvaro.neira.2233@gmail.com>
2026-04-16 14:35:51 -0400

    Update xxxx.py

--- Python/practica/xxxx.py
+++ Python/practica/xxxx.py
@@ -2,3 +2,12 @@ ejercicios
 ---
 1:
 ```python 
+temperatura = int(input(print("ingrese la temperatura actual de su procesador")))
+
+if temperatura <40:
+        print("temperatura normal")
+elif temperatura >75:
+        print("PELIGRO REVISE SUS VENTILADORES!!!")
+else: 
+        print ("temperatura elevada")
+  ```
