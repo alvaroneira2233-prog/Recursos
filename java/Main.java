69d2de640669c42de377317af7cecacf064f8591
alvaro neira <alvaro.neira.2233@gmail.com>
2026-05-07 12:08:42 -0400

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -1,6 +1,12 @@
-public class Main {
+public class main {
      public static void main(String[] args) {
           Lenguaje htlm = new Lenguaje ("HTLML", 1993);
-          
+          htlm.descripcion();
+
+        Lenguaje css = new Lenguaje("CSS", 1996);
+        css.descripcion();
+
+        Lenguaje javaScript = new Lenguaje("JavaScript", 1995);
+        javaScript.descripcion();   
      }
 }
